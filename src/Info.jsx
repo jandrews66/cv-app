9a194b8e9f58dbeb324f532056906a8d9b9eeabb
stephenhackettdelaney <stephenhackettdelaney@gmail.com>
2024-01-02 14:41:35 -0800

    move state higher to allow access from form and preview

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,79 +1,88 @@
 import { useState } from 'react';
 
-export default function InfoForm(){
-   
-   const [user, setUser] = useState({
-        fullName: "",
-        email: "",
-        telephone: ""
-   })
+export default function InfoForm({ user, setUser }) {
 
-   const [submittedInfo, setSubmittedInfo] = useState([user.fullName, user.email, user.telephone])
+    //    moved to higher level 
+    //    const [user, setUser] = useState({
+    //         fullName: "",
+    //         email: "",
+    //         telephone: ""
+    //    })
 
-   const [editMode, setEditMode] = useState(false)
-   
-   function toggleEdit(){
+    // using an isSubmitted value in state now - works for this use case - but ususally form submissions would be storing data/redirecting somewhere etc...
+    // const [submittedInfo, setSubmittedInfo] = useState([user.fullName, user.email, user.telephone])
+
+    const [editMode, setEditMode] = useState(false)
+
+    function toggleEdit() {
         setEditMode(!editMode)
-   }
+    }
 
-   function handleChange(e){
+    function handleChange(e) {
         setUser({
             ...user,
             [e.target.name]: e.target.value
         })
     }
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
-        setSubmittedInfo([user.fullName, user.email, user.telephone])
+        // setSubmittedInfo([user.fullName, user.email, user.telephone])
+        // using user useState from top level parent component (App.js)
+        // just setting isSubmitted true to render <ul> below and in preview
+        setUser({
+            ...user,
+            isSubmitted: true,
+        })
         toggleEdit()
     }
 
     return (
         <>
-        <ul>
-        {submittedInfo.map((info, index) => {
-            return <li key={index}>{info}</li>;
-        })}
-        </ul>
-        {!editMode &&
-            <button onClick={toggleEdit}>Add Info</button>
-        }
-        {editMode &&
-            <form onSubmit={handleSubmit}>        
-            <label>
-                Full Name:
-                <input
-                    name="fullName" 
-                    type="text"
-                    value={user.fullName}
-                    onChange={handleChange}
-                />
-            </label>
-            <label>
-                Email:
-                <input 
-                    name="email"
-                    type="email"
-                    value={user.email}
-                    onChange={handleChange}
+            {!!user.isSubmitted &&
+                (<ul>
+                    {Object.values(user).map((info, index) => {
+                        return <li key={index}>{info}</li>;
+                    })}
+                </ul>)}
+            {!editMode &&
+                <button onClick={toggleEdit}>Add Info</button>
+            }
+            {editMode &&
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Full Name:
+                        <input
+                            name="fullName"
+                            type="text"
+                            value={user.fullName}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <label>
+                        Email:
+                        <input
+                            name="email"
+                            type="email"
+                            value={user.email}
+                            onChange={handleChange}
 
-                />
-            </label>
-            <label>
-                Telephone:
-                <input 
-                    name="telephone"
-                    type="text"
-                    value={user.telephone}
-                    onChange={handleChange}
+                        />
+                    </label>
+                    <label>
+                        Telephone:
+                        <input
+                            name="telephone"
+                            type="text"
+                            value={user.telephone}
+                            onChange={handleChange}
 
-                />
-            </label>
-            <button>Submit</button>
-        </form>     
-        }
+                        />
+                    </label>
+                    <button>Submit</button>
+                </form>
+            }
         </>
-   
+
     )
 }
